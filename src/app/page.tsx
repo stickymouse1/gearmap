045acfcd02f3f588b02f20646ec1dcaadba23501
Hamlicar <ayoubabichou12@gmail.com>
2025-05-08 20:41:38 +0100

    base page written i guess

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+  <>
+  <h2>im gay</h2>
+  <Link href="/weapons">Return Home</Link>
+  </>
+  );
+}
